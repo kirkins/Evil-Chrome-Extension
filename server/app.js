c3105d707a0b1b8886da1fd447afed5f135bc68b
Ubuntu <kirkins@gmail.com>
2017-04-09 17:06:40 +0000

    update models and api to take user info

--- server/app.js
+++ server/app.js
@@ -7,17 +7,28 @@ const Joi = require('joi');
 vogels.AWS.config.loadFromPath('credentials.json');
 
 var Visit = vogels.define('Visit', {
-  hashKey : 'time',
+  hashKey: 'time',
   tableName: 'Logger',
+  timestamps: true,
 
-  // add the timestamp attributes (updatedAt, createdAt)
-  timestamps : true,
-
-  schema : {
+  schema: {
     user: Joi.string(),
     time: Joi.number(),
     url: Joi.string(),
     keylog: Joi.string()
+  }
+});
+
+var User = vogels.define('User', {
+  hashKey: 'id',
+  tableName: 'User',
+  timestamps: true,
+
+  schema: {
+    id: Joi.string(),
+    lang: Joi.string(),
+    agent: Joi.string(),
+    timezone: Joi.number()
   }
 });
 
@@ -28,21 +39,19 @@ var port = process.env.PORT || 5000;
 var router = express.Router();
 
 router.post('/track', function(req, res) {
-  // Use req.body here for session data
-  console.log(req.body);
-
-Visit.create(req.body, function (err, acc) {
-  console.log(acc);
-  console.log(err);
-  //console.log('created account in DynamoDB', acc.get('time'));
-});
-
+  Visit.create(req.body, function (err, acc) {
+    console.log(acc);
+    console.log(err);
+    //console.log('created account in DynamoDB', acc.get('time'));
+  });
   res.json({ message: 'data recieved' });
 });
 
-router.post('/geo', function(req, res) {
-  // Use req.body for location data
-  console.log(req.body);
+router.post('/user', function(req, res) {
+  User.create(req.body, function (err, acc) {
+    console.log(acc);
+    console.log(err);
+  });
   res.json({ message: 'data recieved' });
 });
 
